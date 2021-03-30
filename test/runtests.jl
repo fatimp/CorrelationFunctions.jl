bd0264d109c10a5132f326f49739b741f801c434
Vasily Postnicov <shamaz.mazum@gmail.com>
2021-03-30 10:25:34 +0000

    Test diagonal slicers

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using CorrelationFunctions
 using XUnit
 
-@testset "Random data"     begin include("random.jl") end
-@testset "Checkboard data" begin include("checkboard.jl") end
-@testset "Perlin noise"    begin include("perlin.jl") end
+@testset "Diagonal slicing" begin include("diagonals.jl") end
+@testset "Random data"      begin include("random.jl") end
+@testset "Checkboard data"  begin include("checkboard.jl") end
+@testset "Perlin noise"     begin include("perlin.jl") end
