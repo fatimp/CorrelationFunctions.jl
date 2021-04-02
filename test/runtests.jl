f03d30c0d8498a67c9baec5b1fe2f1c37f01fe6c
Vasily Postnicov <shamaz.mazum@gmail.com>
2021-04-02 12:43:28 +0000

    Test S2 on non-overlapping spheres generated by Poisson process

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,9 @@
 using CorrelationFunctions
 using XUnit
+using PoissonRandom
 
-@testset "Diagonal slicing" begin include("diagonals.jl") end
-@testset "Random data"      begin include("random.jl") end
-@testset "Checkboard data"  begin include("checkboard.jl") end
-@testset "Perlin noise"     begin include("perlin.jl") end
+@testset "Diagonal slicing"        begin include("diagonals.jl") end
+@testset "Random data"             begin include("random.jl") end
+@testset "Checkboard data"         begin include("checkboard.jl") end
+@testset "Perlin noise"            begin include("perlin.jl") end
+@testset "Non-overlapping spheres" begin include("poisson-spheres.jl") end
