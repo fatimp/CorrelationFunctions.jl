68c3c1c2e2feacec68c7404fd68ee9c7af27da55
Vasily Postnicov <shamaz.mazum@gmail.com>
2021-04-29 04:21:06 +0000

    C2 clusterizes only the selected phase

--- src/c2.jl
+++ src/c2.jl
@@ -1,19 +1,18 @@
 """
-    c2(array[; len,][directions,] periodic = false)
+    c2(array, phase[; len,][directions,] periodic = false)
 
 Calculate `C₂` (cluster) correlation function for one-, two- or
 three-dimensional multiphase system.
 
 `C₂(x)` equals to probability that corner elements of a line segment
 with the length `x` cut from the array belong to the same
-cluster. This implementation calculates C2 for all `x`es in the range
-from `1` to `len` which defaults to half of the minimal dimension of
-the array. Also, points which belong to the cluster `0` (pore) are not
-taken into account.
+cluster of the specific phase. This implementation calculates C2 for
+all `x`es in the range from `1` to `len` which defaults to half of the
+minimal dimension of the array.
 
 # Examples
 ```jldoctest
-julia> c2([1,1,1,0,1,1]; len = 6)[:x]
+julia> c2([1,1,1,0,1,1], 1; len = 6)[:x]
 6-element Array{Float64,1}:
  0.8333333333333334
  0.6
@@ -26,12 +25,15 @@ julia> c2([1,1,1,0,1,1]; len = 6)[:x]
 For a list of possible dimensions, see also: [`direction1Dp`](@ref),
 [`direction2Dp`](@ref), [`direction3Dp`](@ref).
 """
-c2(array      :: AbstractArray;
-   directions :: Vector{Symbol} = array |> ndims |> default_directions,
-   len        :: Integer = (array |> size |> minimum) ÷ 2,
-   periodic   :: Bool = false) =
-       s2(label_components(array),
-          InseparableIndicator((x, y) -> x == y != 0);
-          len        = len,
-          directions = directions,
-          periodic   = periodic)
+function c2(array      :: AbstractArray,
+            phase;
+            directions :: Vector{Symbol} = array |> ndims |> default_directions,
+            len        :: Integer = (array |> size |> minimum) ÷ 2,
+            periodic   :: Bool = false)
+    field = map(x -> x == phase, array)
+    s2(label_components(field),
+       InseparableIndicator((x, y) -> x == y != 0);
+       len        = len,
+       directions = directions,
+       periodic   = periodic)
+end
