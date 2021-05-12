4649df5c407a23a09a78eb7a568eb781d3c5096e
Vasily Postnicov <shamaz.mazum@gmail.com>
2021-05-12 20:06:34 +0300

    Deploy docs to GH Pages

--- docs/make.jl
+++ docs/make.jl
@@ -3,3 +3,5 @@ using CorrelationFunctions
 
 makedocs(sitename = "CorrelationFunctions.jl documentation",
          format = Documenter.HTML(prettyurls = false))
+
+deploydocs(repo = "github.com/shamazmazum/CorrelationFunctions.jl.git")
