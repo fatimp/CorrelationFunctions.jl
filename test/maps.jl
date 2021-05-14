0705025b6325d144bf5bb77bcf9b80dfe412d849
Vasily Postnicov <shamaz.mazum@gmail.com>
2021-05-14 13:24:19 +0300

    Check other correlation maps for 2D case

--- test/maps.jl
+++ test/maps.jl
@@ -12,19 +12,29 @@ cut_direction(array :: AbstractMatrix, :: Val{:xy_main}) =
 cut_direction(array :: AbstractMatrix, :: Val{:xy_anti}) =
     let n = size(array, 2); [array[n - i + 1, i] for i in 1:n] end
 
-@testset "s2 map" begin
-    testset = two_phase_noise_2d()
-    for p in (false, true)
-        directional = Directional.s2(testset, 1; len = 50, periodic = p,
-                                     directions = [:x, :y, :xy_main, :xy_anti])
-        cmap = Map.corr_function_map(testset, Map.s2; periodic = p)
-        @test cut_direction(cmap, :x) ≈ directional[:x]
-        @test cut_direction(cmap, :y) ≈ directional[:y]
+function testmap(func, mapfunc)
+    @testset "$func map" begin
+        testset = two_phase_noise_2d()
+        for p in (false, true)
+            directional = func(testset, 1; len = 50, periodic = p,
+                               directions = [:x, :y, :xy_main, :xy_anti])
+            cmap = mapfunc(testset, p)
+            @test cut_direction(cmap, :x) ≈ directional[:x]
+            @test cut_direction(cmap, :y) ≈ directional[:y]
 
-        # TODO: Fix periodic diagonals
-        if !p
-            @test cut_direction(cmap, :xy_main) ≈ directional[:xy_main]
-            @test cut_direction(cmap, :xy_anti) ≈ directional[:xy_anti]
+            # TODO: Fix periodic diagonals
+            if !p
+                @test cut_direction(cmap, :xy_main) ≈ directional[:xy_main]
+                @test cut_direction(cmap, :xy_anti) ≈ directional[:xy_anti]
+            end
         end
     end
 end
+
+mapfn(map) = (x, p) -> Map.corr_function_map(x, map; periodic = p)
+# ! FIXME: l2 is broken
+testmap(Directional.s2,       mapfn(Map.s2))
+testmap(Directional.c2,       mapfn(Map.c2))
+testmap(Directional.surfsurf, mapfn(Map.ss))
+testmap(Directional.surfvoid,
+        (x -> let n = size(x, 2); x[:, n÷2 + 1:end] end) ∘ mapfn(Map.sv))
