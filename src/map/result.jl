cb1c78a729423f051104a194ea7cad83c0b10fa6
LexaLutyi <liotbiu1@gmail.com>
2021-06-03 15:23:19 +0300

    fix periodic point point

--- src/map/result.jl
+++ src/map/result.jl
@@ -58,17 +58,32 @@ function dir_ixs(cfmap, dir::Symbol)
 end
 
 
+function shiftmap(cfmap::CFMap)
+    res = cfmap.result
+    ns = size(res)
+    ms = ns .÷ 2
+    raw_ixs = map(m -> -m:m, ms)
+    ixs = map((rix, n) -> mod.(rix, n) .+ 1, raw_ixs, ns)
+    res[ixs...], raw_ixs
+end
+
+
 function itp_map(cfmap)
-    xs = map((ix, z) -> ix .- z, axes(cfmap.result), cfmap.zero_index)
-    interpolate(xs, cfmap.result, Gridded(Linear()))
+    if cfmap.cf_type == :periodic_point_point
+        shiftres, xs = shiftmap(cfmap)
+        itp = interpolate(xs, shiftres, Gridded(Linear()))
+        extrapolate(itp, Periodic())
+    else
+        xs = map((ix, z) -> ix .- z, axes(cfmap.result), cfmap.zero_index)
+        itp = interpolate(xs, cfmap.result, Gridded(Linear()))
+    end
 end
 
 
 function dir_from_map(cfmap, dir)
     way = dir_ixs(cfmap, dir)
     itp = itp_map(cfmap)
-    etp = extrapolate(itp, Periodic())
-    etp.(way...)
+    itp.(way...)
 end
 
 
@@ -112,13 +127,12 @@ function mean_dir(cfmap)
     result = zeros(n)
     
     itp = itp_map(cfmap)
-    extp = extrapolate(itp, Periodic())
     
     dirs = all_dirs(cfmap)
     for dir in dirs
         scale = maximum(abs, dir) / norm(dir)
         way = dir_ixs(cfmap, dir) .* scale
-        result += extp.(way...)
+        result += itp.(way...)
     end
     result ./ length(dirs)
 end
@@ -136,7 +150,7 @@ function restore_full_map(cfmap::CFMap{T,N}) where T where N
         [selectdim(reversed, N, 1:n - 1) res]
 
     elseif cfmap.cf_type == :periodic_point_point
-        circshift(res, div.(size(res) .- 1, 2))
+        shiftmap(cfmap)[1]
 
     else
         error()
