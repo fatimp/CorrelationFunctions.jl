94fd5d55e3eff79747a858a432951718eb21860b
Vasily Postnicov <shamaz.mazum@gmail.com>
2021-06-03 04:14:20 +0000

    Fix mono font

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,9 @@ using Documenter
 using CorrelationFunctions
 
 makedocs(sitename = "CorrelationFunctions.jl documentation",
-         format = Documenter.HTML(prettyurls = false))
+         format   = Documenter.HTML(prettyurls = false,
+                                    assets     = [
+                                        "assets/code.css"
+                                    ]))
 
 deploydocs(repo = "github.com/shamazmazum/CorrelationFunctions.jl.git")
