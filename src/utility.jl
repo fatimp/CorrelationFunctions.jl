d17093612c50626eb7482dde836d3ec34d5c0fe9
Vasily Postnicov <shamaz.mazum@gmail.com>
2021-07-09 09:07:31 +0300

    Reinvent Euclidean distance transform.
    This implementation also works on torus

--- src/utility.jl
+++ src/utility.jl
@@ -40,7 +40,10 @@ function read_cuboid(cfgpath :: String)
     return data
 end
 
-# Component labeling
+######################
+# Component labeling #
+######################
+
 # Two components are considered connected if they have manhattan_dist == 1
 manhattan_dist(x :: NTuple{N, Int}, y :: NTuple{N, Int}) where N =
     sum(abs.(x .- y))
@@ -130,3 +133,111 @@ function Images.label_components(input :: AbstractArray{T, N},
 end
 
 Images.label_components(input, :: Plane) = Images.label_components(input)
+
+################################
+# Euclidean distance transform #
+################################
+
+# This code can be rewritten into more beautiful implementation.
+# The bad thing is that Julia is against any beauty.
+
+function edt(array    :: AbstractArray{Bool},
+             topology :: Topology)
+    len = array |> length |> Float64
+    binary = map(x -> len^2*(1-x), array)
+    return edt!(binary, topology) .|> sqrt
+end
+
+function lower_envelope(array :: AbstractVector{Float64})
+    # This code is Fortran-like.
+    # Proper data structures like lists are required to make it more understandable.
+    # Unfortunately, Julia does not have them, so prepare to suffer.
+    len = length(array)
+    v = zeros(Int, len)
+    z = zeros(Float64, len+1)
+
+    # Compute the lower envelope of family of parabolas
+    k = 1
+    v[1] = 1
+    z[1] = -Inf
+    z[2] = Inf
+
+    for i in 2:len
+        s = 0
+        while true
+            j = v[k]
+            s = ((array[i] + i^2) - (array[j] + j^2))/(2i - 2j)
+            if s > z[k]
+                break
+            end
+            k = k - 1
+        end
+        k = k + 1
+        v[k] = i
+        z[k] = s
+        z[k+1] = Inf
+    end
+
+    return v, z
+end
+
+function edt!(array :: AbstractVector{Float64}, :: Plane)
+    v, z = lower_envelope(array)
+    len = length(array)
+
+    # Compute EDT by selecting a parabola which belongs to a lower
+    # envelope for this line segment
+    k = 1
+    for i = 1:len
+        while z[k+1] < i
+            k = k + 1
+        end
+        j = v[k]
+        array[i] = array[j] + (i - j)^2
+    end
+
+    return array
+end
+
+function edt!(array :: AbstractVector{Float64}, :: Torus)
+    # Just do three times more work than for ::Plane version.
+    # This can be optimized in the future.
+    len = length(array)
+    a2 = vcat(array, array, array)
+    edt!(a2, Plane())
+    array .= a2[len+1:2len]
+    return array
+end
+
+function edt!(array :: AbstractMatrix{Float64}, topology :: Topology)
+    x, y = size(array)
+    local edtfn!(x) = edt!(x, topology)
+
+    columns = (view(array, :, i) for i in 1:y)
+    foreach(edtfn!, columns)
+
+    rows = (view(array, i, :) for i in 1:x)
+    foreach(edtfn!, rows)
+
+    return array
+end
+
+function edt!(array :: AbstractArray{Float64, 3}, topology :: Topology)
+    x, y, z = size(array)
+    local edtfn!(x) = edt!(x, topology)
+
+    planes = (view(array, :, :, i) for i in 1:z)
+    foreach(edtfn!, planes)
+
+    sides = (view(array, i, j, :) for i in 1:x for j in 1:y)
+    foreach(edtfn!, sides)
+
+    return array
+end
+
+# Use faster implementation from ImageMorphology with Plane topology.
+Images.distance_transform(array :: AbstractArray{Bool}, :: Plane) =
+    array |> Images.feature_transform |> Images.distance_transform
+
+Images.distance_transform(array :: AbstractArray{Bool}, :: Torus) =
+    edt(array, Torus())
