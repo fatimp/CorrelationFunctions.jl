d6d12050c98679e95e78bac99799eff0065e8d54
Vasily Postnicov <shamaz.mazum@gmail.com>
2021-09-07 10:43:34 +0000

    Another function for reading data

--- src/utility.jl
+++ src/utility.jl
@@ -40,6 +40,32 @@ function read_cuboid(cfgpath :: String)
     return data
 end
 
+@doc raw"""
+    read_cuboid(datapath :: String, side, dim)
+
+Read 3D array from a disk. The data on the disk must be in binary
+format, one octet per sample. Totally, there must be $side^{dim}$
+octets which are read into $side \times side \times \dots \times side$
+array.
+"""
+function read_cuboid(datapath :: String,
+                     side     :: Integer,
+                     dim      :: Integer)
+    s = stat(datapath)
+    if s.size != prod(side^dim)
+        error("File size mismatch. Got $(s.size), expected $(side^dim)")
+    end
+
+    dimensions = fill(side, dim) |> Tuple
+    data = Array{Int8, dim}(undef, dimensions...)
+
+    open(datapath) do input
+        read!(input, data)
+    end
+
+    return data
+end
+
 ######################
 # Component labeling #
 ######################
