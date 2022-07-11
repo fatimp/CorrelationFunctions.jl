c3f31d81bdebe9c4b5f4f00c9cc3f925cab1a5ee
Vasily Postnicov <shamaz.mazum@gmail.com>
2022-07-11 17:03:45 +0300

    Move all methods of dir_from_map to one place

--- src/CorrelationFunctions.jl
+++ src/CorrelationFunctions.jl
@@ -55,7 +55,6 @@ include("map/s2_map.jl")
 include("map/c2_map.jl")
 include("map/ss_map.jl")
 include("map/sv_map.jl")
-include("map/dir_from_map.jl")
 
 export l2, s2, c2, surfsurf, surfvoid, 
     dir_from_map, restore_full_map, mean_dir
