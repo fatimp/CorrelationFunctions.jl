4b662819ff86ee2fa813806e2f89b9c8344773f4
Vasily Postnicov <shamaz.mazum@gmail.com>
2022-12-12 11:28:56 +0300

    Fix map tests

--- test/maps.jl
+++ test/maps.jl
@@ -1,42 +1,38 @@
-const cfs = [:s2, :c2, :surfsurf, :surfvoid]
+const cfs  = Dict(
+    1 => [:s2, :c2],
+    2 => [:s2, :c2, :surfsurf, :surfvoid],
+    3 => [:s2, :c2, :surfsurf, :surfvoid]
+)
+
 const dirs = Dict(
     1 => [:x],
     2 => [:x, :y, :xy, :yx],
     3 => [:x, :y, :z]
 )
 
-
 function test_cf_on_img(img, cf_map, cf_dir)
+    directions = dirs[ndims(img)]
+
     for periodic in [true, false]
         cmap = cf_map(img, true; periodic)
-        cdir = cf_dir(img, true; len=size(img, 1), periodic, directions=dirs[ndims(img)])
+        cdir = cf_dir(img, true; len=size(img, 1), periodic, directions)
 
         for (direction, data) in cdir
-            if Symbol(cf_map) == :l2
-                @test Map.dir_from_map(cmap, direction) ≈ data
-            else
-                @test Map.dir_from_map(cmap, direction; periodic) ≈ data
-            end
+            @test Map.dir_from_map(cmap, direction; periodic) ≈ data
         end
     end
 end
 
-
 function test_all()
-    dims = [1, 2, 3]
-    n = 10
-
-    for dim in dims
-        ns = Tuple(ones(Int, dim) * n)
-        img = rand(Bool, ns)
+    for dim in 1:3
+        img = rand(Bool, (10 for _ in 1:dim)...)
         
-        for cf in cfs
+        for cf in cfs[dim]
             @testset "random $(dim)D image, $(cf)" begin
                 test_cf_on_img(img, Map.eval(cf), Directional.eval(cf))
             end
         end
     end
 end
-
 
 test_all()
