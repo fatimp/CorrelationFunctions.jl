7ce755d114f79b8110e3a94691ecc9efa9f90796
Vasily Postnicov <shamaz.mazum@gmail.com>
2023-02-13 14:16:17 +0300

    Adapt to the recent changes in master

--- src/directional/surface3.jl
+++ src/directional/surface3.jl
@@ -13,13 +13,13 @@ function surf3(array        :: AbstractArray, phase;
                periodic     :: Bool                  = false,
                len = (array |> size |> minimum) ÷ 2)
     topology = periodic ? Torus() : Plane()
-    edges = extract_edges(array .== phase, EdgeFilter(topology, ErosionKernel(5)))
+    edges = extract_edges(array .== phase, ErosionKernel(5), topology)
 
     # TODO: Better renormalization?
     scale = maximum(edges)
     scale = iszero(scale) ? scale : 1/scale
     edges = scale * edges
-    
+
     op(x, y, z) = x * y * z
     calc_s3(plane) = plane => autocorr3_plane(edges, op, plane, topology, len) / scale^2
     return Dict{AbstractPlane, Matrix{Float64}}(map(calc_s3, planes))
