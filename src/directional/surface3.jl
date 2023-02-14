b317bb162d2b5e9b920e5e3892bfc46bfd1b2f99
Vasily Postnicov <shamaz.mazum@gmail.com>
2023-02-14 08:56:05 +0300

    surf3: Allow arbitrary ErosionFilter

--- src/directional/surface3.jl
+++ src/directional/surface3.jl
@@ -1,19 +1,25 @@
 @doc raw"""
-    surf3(array, phase[; planes :: Vector{AbstractPlane}, len, periodic = false])
+    surf3(array, phase[; planes :: Vector{AbstractPlane},
+                         len, periodic = false, filter :: ErosionKernel])
 
 Calculate surface-surface-surface ($F_{sss}$) correlation function.
 
 This function is is internally calculated using `s3` and hence uses
 the same sampling pattern and returns a result in the same format.
 
-See also: [`s3`](@ref), [`AbstractPlane`](@ref).
+You can chose how an edge between phases is selected by passing
+`filter` argument of type `Utilities.ErosionKernel`.
+
+See also: [`s3`](@ref), [`AbstractPlane`](@ref),
+[`ErosionKernel`](@ref).
 """
 function surf3(array        :: AbstractArray, phase;
                planes       :: Vector{AbstractPlane} = default_planes(array),
                periodic     :: Bool                  = false,
+               filter       :: ErosionKernel         = ErosionKernel(5),
                len = (array |> size |> minimum) ÷ 2)
     topology = periodic ? Torus() : Plane()
-    edges = extract_edges(array .== phase, ErosionKernel(5), topology)
+    edges = extract_edges(array .== phase, filter, topology)
 
     # TODO: Better renormalization?
     scale = maximum(edges)
