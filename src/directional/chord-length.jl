1907f84051033ca2b999de5aa8ccc962ae9409d2
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-04-27 13:01:37 +0300

    Remove an explicit type declaration
    This case is fixed as in Julia 1.10.1. No guarantees it will not be
    broken in the future, but as for now we do not need the declarations
    and a preceding comment.

--- src/directional/chord-length.jl
+++ src/directional/chord-length.jl
@@ -37,18 +37,8 @@ function chord_length(array :: AbstractArray, phase, direction :: AbstractDirect
 
     ph_slices   = slice_generators(ph,   false, direction)
     edge_slices = slice_generators(edge, false, direction)
-    # KLUDGE: Julia cannot infer types here.
-    #
-    # We restrict ourselves to accept arrays only of type Array or
-    # BitArray. Doing so, we expect slices to be of type BitVector
-    # (slice_generators(...) returns an array of the same type as
-    # its first argument). I thinks those two types will cover all
-    # our needs and if not, we'll get a runtime error.
-    #
-    # Why does Julia infer types in all other places where
-    # slice_generators() is used? Why does @code_warntype fails to
-    # give any sensible information?
-    for (ph_slice :: BitVector, edge_slice :: BitVector) in Iterators.zip(ph_slices, edge_slices)
+
+    for (ph_slice, edge_slice) in Iterators.zip(ph_slices, edge_slices)
         len = 0
         startonedge = false
         # ph_slice and edge_slice has the same shape
