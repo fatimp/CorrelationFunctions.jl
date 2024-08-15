2825d9eb6cb786b4210b823b3ab12c4478065c84
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-08-15 09:49:54 +0300

    Remove image rotation (replaced with arbitrary patterns)

--- src/CorrelationFunctions.jl
+++ src/CorrelationFunctions.jl
@@ -1,11 +1,9 @@
 module CorrelationFunctions
 
 module Utilities
-using Quaternions
 using CircularArrays: CircularArray
 using OffsetArrays: centered
-using StaticArrays: SVector, SMatrix
-using LinearAlgebra: norm, eigvecs, det, Symmetric
+using LinearAlgebra: norm
 using FFTW: fft, plan_rfft, irfft
 using Statistics: mean
 using CUDA: CuArray
@@ -17,27 +15,20 @@ include("utility/rawreader.jl")
 include("utility/lowfreq_energy_ratio.jl")
 include("utility/images.jl")
 include("utility/directions.jl")
-include("utility/infinite_padded_views.jl")
-include("utility/rotation.jl")
-include("utility/anisotropy.jl")
 include("utility/pattern.jl")
 
-export read_cuboid, lowfreq_energy_ratio,
-    distance_transform, label_components, extract_edges,
-    AbstractKernel, ConvKernel, ErosionKernel,
-    AbstractTopology, Torus, Plane, AbstractDirection, DirX, DirY,
-    DirZ, DirXY, DirYX, DirXZ, DirZX, DirYZ, DirZY, DirXYZ, DirXZY,
-    DirYXZ, DirZYX, check_direction, check_rank,
-    maybe_upload_to_gpu, AbstractRotation, VectorRotation,
-    MatRotation, make_rotation, rotate_array, detect_anisotropy,
-    RightTrianglePattern, AbstractPlane, PlaneXY, PlaneXZ, PlaneYZ,
-    right_triangles
+export read_cuboid, lowfreq_energy_ratio, distance_transform,
+    label_components, extract_edges, AbstractKernel, ConvKernel,
+    ErosionKernel, AbstractTopology, Torus, Plane, AbstractDirection,
+    DirX, DirY, DirZ, DirXY, DirYX, DirXZ, DirZX, DirYZ, DirZY,
+    DirXYZ, DirXZY, DirYXZ, DirZYX, check_direction, check_rank,
+    maybe_upload_to_gpu, RightTrianglePattern, AbstractPlane, PlaneXY,
+    PlaneXZ, PlaneYZ, right_triangles
 end
 
 module Directional
 using ..Utilities
 using Statistics: mean, std
-using LinearAlgebra: normalize
 using Base.Iterators
 using FFTW: plan_rfft, plan_irfft, rfft, irfft
 using CircularArrays: CircularArray
@@ -66,7 +57,6 @@ end # Directional
 
 module Map
 using ..Utilities
-using LinearAlgebra: norm
 using CUDA: CuArray
 using FFTW: rfft, irfft, ifftshift, plan_rfft
 import CUDA.CUFFT
--- test/runtests.jl
+++ test/runtests.jl
@@ -7,13 +7,11 @@ using Base.Iterators: zip, drop, countfrom
 using StatsBase: mean, std, fit, Histogram
 using ImageFiltering: centered
 using LinearAlgebra: norm, normalize
-using StaticArrays: SVector
 using CUDA: CuArray
 
 # Test utilities
 include("utility.jl")
 
-@testset "Rotations"           begin include("rotations.jl") end
 @testset "Short sequences"     begin include("short.jl") end
 @testset "Random data"         begin include("random.jl") end
 @testset "Checkboard data"     begin include("checkboard.jl") end
