4533927947b090dcf49faf2e3f2fbefe9b9c2d21
Vasily Postnicov <shamaz.mazum@gmail.com>
2024-08-23 10:08:16 +0300

    Fix a typo in test

--- test/value-noise.jl
+++ test/value-noise.jl
@@ -108,7 +108,6 @@ end
     end
 end
 
-#=
 @testset "Check F_{ss} calculation with a mask (Directional)" begin
     noise = two_phase_noise_3d()
     # Add zero padding because of different boundary conditions in extract_edges()
@@ -121,12 +120,10 @@ end
 
     for phase in [false, true]
         cf1 = D.surf2(big,    phase, U.DirX(); len = 60, mode = U.Mask(mask))
-        cf2 = D.surf2(padded, phase, U.DirY(); len = 60, mode = U.NonPeriodic())
-        @test isapprox(cf1, cf2; atol = 1e-3)
-        #@test cf1 ≈ cf2
+        cf2 = D.surf2(padded, phase, U.DirX(); len = 60, mode = U.NonPeriodic())
+        @test cf1 ≈ cf2
     end
 end
-=#
 
 @testset "Check F_{sv} calculation with a mask (Map)" begin
     noise = two_phase_noise_3d()
